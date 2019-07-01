fa2613af6ebb79370fe05c1b973eadebb2890295
JimmyCN1 <chibueze.e.njoku@gmail.com>
2019-07-01 18:06:37 +1000

    sum all primes

---
+++ sumAllPrimes.js
@@ -0,0 +1,23 @@
+// this algorithm will sum all primes up to and including the number passed into the function
+
+function sumPrimes(num) {
+  let initialNumberSet = [];
+  for (let i = 2; i <= num; i++) {
+    initialNumberSet.push(i);
+  }
+  let filteredNumberSet = [...initialNumberSet];
+  let prime = 0;
+  for (let i = 0; i < filteredNumberSet.length; i++) {
+    prime = filteredNumberSet[i];
+    filteredNumberSet = filteredNumberSet.filter(
+      number => number === prime || number % prime !== 0
+    );
+  }
+  let sumOfPrimes = filteredNumberSet.reduce((sum, prime) => sum + prime);
+  console.log(sumOfPrimes);
+  return sumOfPrimes;
+}
+
+sumPrimes(10); // should return 17
+sumPrimes(100); // should return 1060
+sumPrimes(977); // should return 73156
